3c4b2b080842a704202d254c057382d3177cda34
Paulo Mota <paaulowiz@gmail.com>
2021-01-27 23:55:17 -0300

    favicon and send contact test

--- main.py
+++ main.py
@@ -1,43 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for,request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def new():
+def my_home():
     return render_template('index.html')  # if you want to render a .html file,
     # import render_template from flask and use
     # render_template("index.html") here.
 
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
-
-
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
-
-
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
-
-
-@app.route('/contact.html')
-def contact():
-    return render_template('components.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return data
+    else:
+        return 'something happend wrong'
 
 
 if __name__ == '__main__':
